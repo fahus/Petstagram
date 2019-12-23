5872933bdd5092463ed5ae9efac7e725deac5f7c
fahus <faduma.h@yahoo.com>
2019-12-23 21:05:22 +0000

    user can update profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
 
   def show
-  end 
-end
+  end
+
+  def update
+    current_user.update(user_params)
+    redirect_to current_user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :name, :website,
+      :bio, :email, :phone, :gender)
+    end
+  end
