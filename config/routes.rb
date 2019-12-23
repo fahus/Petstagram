f968246a1cc89f9d8f816cb25a012b1fb5dabb35
fahus <faduma.h@yahoo.com>
2019-12-23 20:23:57 +0000

    add user profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
+  
+    root to: 'home#index'
+    devise_for :users
 
-root to: 'home#index'
+  resources :users, only: [:show]
+    get 'home/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
