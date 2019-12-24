7eefb66309d462c6b0e420ce9ef75a13796a741c
fahus <faduma.h@yahoo.com>
2019-12-24 19:39:22 +0000

    user can upload posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     devise_for :users
 
   resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create]
     get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
